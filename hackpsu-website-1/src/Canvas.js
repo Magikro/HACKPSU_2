d726286f47bc8ce87cf8950699fc781d5b13d6c7
Ethan <emeraldtails.em@gmail.com>
2020-11-07 11:37:00 -0500

    adding drawing function, not done

--- hackpsu-website-1/src/Canvas.js
+++ hackpsu-website-1/src/Canvas.js
@@ -4,11 +4,41 @@ const Canvas = props => {
 
     const canvasRef = useRef(null)
 
+    window.addEventListener('load', ()=> {
+
+        resize();
+        document.addEventListener('mousedown', startPainting);
+        document.addEventListener('mouseup', stopPainting);
+        document.addEventListener('mousemove', sketch);
+        window.addEventListener('resize', resize);
+
+    });
+
     useEffect(() => {
         const canvas = canvasRef.current
-        const context = canvas.getContext('2d')
-        context.fillStyle = '#000000'
-        context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+        const ctx = canvas.getContext('2d')
+
+        function resize(){
+            ctx.canvas.width = window.innerWidth;
+            ctx.canvas.height = window.innerHeight;
+        }
+
+        let coord = {x:0 , y:0};
+
+        let paint = false;
+
+        function getPosition(event){
+            coord.x = event.clientX - canvas.offsetLeft;
+            coord.y = event.clientY - canvas.offsetTop;
+        }
+
+        function startPainting(event){
+            paint = true;
+            getPosition(event);
+        }
+
+        ctx.fillStyle = '#000000'
+        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
     }, [])
 
     return <canvas ref={canvasRef} {...props}/>
